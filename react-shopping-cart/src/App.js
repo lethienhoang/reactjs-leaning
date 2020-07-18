f7c7c7f9cf50d2161cbf5d08b3ee77f518b96f37
lethienhoang <45428011+lethienhoang@users.noreply.github.com>
2020-07-18 12:06:43 +0700

    implement products filter and ordering

--- react-shopping-cart/src/App.js
+++ react-shopping-cart/src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React from "react";
 import data from "./data.json";
 import Products from "./components/products.component";
+import Filter from "./components/filter.component";
 
 class App extends React.Component {
   constructor() {
@@ -8,9 +9,45 @@ class App extends React.Component {
     this.state = {
       products: data.products,
       size: "",
-      sort: ""
+      sort: "",
     };
   }
+
+  sortProducts = (event) => {
+    const sort = event.tart.value;
+
+    this.setState((state) => ({
+      sort: event.target.value,
+      products: this.state.products
+        .slice()
+        .sortProducts((item, item1) =>
+          sort === "lowest"
+            ? item.price < item1.price
+              ? 1
+              : -1
+            : sort === "highest"
+            ? item.price > item1.price
+              ? 1
+              : -1
+            : item._id > item1._id
+            ? 1
+            : -1
+        ),
+    }));
+  };
+
+  filterProducts = (event) => {
+    if (event.target.value === "") {
+      this.setState({ size: event.tart.value, products: data.products });
+    } else {
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(event.target.value) >= 0
+        ),
+      });
+    }
+  };
 
   render() {
     return (
@@ -21,14 +58,19 @@ class App extends React.Component {
         <main>
           <div className="content">
             <div className="main">
+              <Filter
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.size.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}
+              ></Filter>
               <Products products={this.state.products}></Products>
             </div>
             <div className="sidebar">Cart Items</div>
           </div>
         </main>
-        <footer>
-          All right is reserved;
-        </footer>
+        <footer>All right is reserved;</footer>
       </div>
     );
   }
